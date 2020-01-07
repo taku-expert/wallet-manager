4de1d02649e6de5c33d4b3020b4530a35bcb197d
taku-expert <prime.stkvkp@gmail.com>
2020-01-07 21:57:08 +0900

    入力された金額の取得

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -3,8 +3,17 @@ class WalletsController < ApplicationController
   def index
     @wallet = Wallet.new
     @wallets = Wallet.all.order("created_at DESC")
+    
     @yellow = Wallet.all.sum(:price)
-    @purple = Wallet.find[:]
+    
+    @purples = Wallet.where(color: '1')
+    @purple = @purples.all.sum(:price)
+
+    @blues = Wallet.where(color: '2')
+    @blue = @blues.all.sum(:price)
+
+    @pinks = Wallet.where(color: '3')
+    @pink = @pinks.all.sum(:price)
   end
 
   def create
