4c612a6730948fd8b2aa2206a4fffe886ee6e1ac
taku-expert <prime.stkvkp@gmail.com>
2020-01-10 12:15:56 +0900

    計算結果の表示

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,11 +1,11 @@
 class WalletsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+
+  before_action :move_to_index, except: [:home, :index, :create]
 
   def home
   end
 
   def index
-
     @user = User.find(current_user.id)
 
     @wallet = Wallet.new
@@ -18,16 +18,38 @@ class WalletsController < ApplicationController
     @pink = @wallets.where(color: '3').sum(:price)
 
     # ( 設定された残高 / 入力金額の総合計 ) をして割合を算出
+    # 算出(Calculation) 割合(Percentage) 余り(Remainder)
     @balance = @user.balance
-    @calculation = ( @balance.to_f / @yellow ).to_f
-    @percentage = 100 / @calculation
+    @yellow_cal = ( @balance.to_f / @yellow ).to_f
+    @yellow_per = 100 / @yellow_cal
 
+    # DBが空の場合は計算ができないため結果を"0"として表示
+    if @yellow == 0
+      @purple_per_total = 0
+      @blue_per = 0
+      @pink_per = 0
+    else
+      @purple_cal = ( @yellow.to_f / @purple ).to_f
+      @purple_per = 100 / @purple_cal
+
+      @blue_cal = ( @yellow.to_f / @blue ).to_f
+      @blue_per = 100 / @blue_cal
+
+      @pink_cal = ( @yellow.to_f / @pink ).to_f
+      @pink_per = 100 / @pink_cal
+
+      # 端数分は@purpleに足すこととする。
+      @rem = (100 - @purple_per.floor - @blue_per.floor - @pink_per.floor )
+      @purple_per_total = @purple_per + @rem
+    end
   end
 
   def create
     @wallet = Wallet.new(wallet_params)
     @wallet.save
+    redirect_to root_path
   end
+
 
   private
   def wallet_params
